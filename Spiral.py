e85a0d8d0e21f76d3d5599331f4766a16231fbc5
mreichel127 <112231105+mreichel127@users.noreply.github.com>
2022-09-02 18:57:12 -0500

    Add files via upload

--- Spiral.py
+++ Spiral.py
@@ -1,22 +1,22 @@
 #  File: Spiral.py
 
-#  Description:
+#  Description: created a number spiral and added certain values around a given number
 
-#  Student Name:
+#  Student Name: Madeline Reichel
 
-#  Student UT EID:
+#  Student UT EID: mr57723
 
-#  Partner Name:
+#  Partner Name: Jennifer Kim
 
-#  Partner UT EID:
+#  Partner UT EID: ck27924
 
 #  Course Name: CS 313E
 
-#  Unique Number: 
+#  Unique Number: 52520
 
-#  Date Created:
+#  Date Created: 08/29/2022
 
-#  Date Last Modified:
+#  Date Last Modified: 09/02/2022
 
 # Input: n is an odd integer between 1 and 100
 # Output: returns a 2-D list representing a spiral
@@ -56,19 +56,25 @@ def create_spiral(n):
 # Output: returns an integer that is the sum of the
 #         numbers adjacent to n in the spiral
 #         if n is outside the range return 0
+
 def sum_adjacent_numbers(spiral, newInput):
+    r = 0
+    c = 0
     for i in range(len(spiral)):
         for j in range(len(spiral[i])):
-            if newInput == j: 
-                numIndex = [i][j]
-                print(numIndex)
+            if newInput == spiral[i][j]: 
+                r = i
+                c = j
     sumOutput = 0
-    for i in range(8):
-        try:
-            sumOutput += spiral[0][0]
-        except IndexError():
-            pass
-    return sumOutput
+    for a in range(r-1, r+2):
+        for b in range(c-1, c+2):
+            if a == -1 or b == -1:
+                continue
+            try:
+                sumOutput += spiral[a][b]
+            except: 
+                    sumOutput += 0
+    return int(sumOutput) - int(newInput)
     
 
 def main():
@@ -76,13 +82,9 @@ def main():
     file = sys.stdin.read()
     fileList = file.split("\n")
     fileList.remove('')
-    #print(fileList)
     
     spiral = create_spiral(int(fileList[0]))
-    
-    output = sum_adjacent_numbers(spiral, fileList[1])
-    
-    for r in spiral:
-        print(r)
+    for num in range(1, len(fileList)):
+        print(sum_adjacent_numbers(spiral, int(fileList[num])))
 if __name__ == "__main__":
     main()
